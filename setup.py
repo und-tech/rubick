4296a085b46db6fdfc9daa553876ef3581502036
wilsson <wilsonft83@gmail.com>
2019-02-12 16:52:40 -0500

    fix(PyYAML): add fix to version PyYAML to 4.2b1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires = ["click==6.7",
                         "Jinja2==2.10",
                         "GitPython==2.1.11",
-                        "PyYAML==3.12",
+                        "PyYAML==4.2b1",
                         "terminaltables==3.1.0"],
     classifiers = [
         "Programming Language :: Python :: 3",
